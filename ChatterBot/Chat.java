534badbbc03861e373b8ca45de4fd4509140f608
Liran-tal <taliran44@gmail.com>
2022-05-15 12:04:29 +0300

    changes to chatter bot

--- ChatterBot/Chat.java
+++ ChatterBot/Chat.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
+class Chat {
     public static void main(String[] args) {
-        String[] repliesToIllegalRequest = {"what ", "say I should say "};
+        String[][] repliesToIllegalRequest = {
+            {"what ", "say I should say "},
+            { "whaaat ", "say say " }
+        };
         ChatterBot[] bots = {
-          new ChatterBot(repliesToIllegalRequest), 
-          new ChatterBot(repliesToIllegalRequest)
+          new ChatterBot(repliesToIllegalRequest[0]), 
+          new ChatterBot(repliesToIllegalRequest[1])
         };
         String statement = "hello";
         Scanner scanner = new Scanner(System.in);
         
-        for (int i = 0; ; i = (i + 1) % bots.length) {
+        for (int i = 0; true; i = (i + 1) % bots.length) {
             statement = bots[i].replyTo(statement);
-            System.out.println("bot " + i + " said: " +  statement);
+            System.out.println("bot " + (i + 1) + " said: " +  statement);
             scanner.nextLine();
+            scanner.close();
         }
     }
 }
