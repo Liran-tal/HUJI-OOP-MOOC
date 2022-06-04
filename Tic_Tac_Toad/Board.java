f018f521bbff19a7c6bfe601af60d24752d507a5
Liran-tal <taliran44@gmail.com>
2022-06-04 11:12:40 +0300

    tic-tac-toe board signatures

---
+++ Tic_Tac_Toad/Board.java
@@ -0,0 +1,26 @@
+// import Mark 
+
+// class Board {
+
+  // public Board ();
+  // public static final int SIZE = 3;
+  // public static final int WIN_STREAK = 3;
+
+//   public boolean putMark (Mark mark, int row, int col) {
+
+//   };
+
+//   public Mark getMark (int row, int col) {
+
+//   };
+
+//   boolean isGmaeEnded () {
+
+//   };
+
+//   Enum
+
+// public getSize () {
+  
+// };
+// }
